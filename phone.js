cde20e810d29b7364f073fe3bac0a36b18faee67
VTCodeCraft <work.vishesh12.05@gmail.com>
2025-07-08 12:35:40 +0530

    Save login credentials to chrome.storage.local upon successful login

--- phone.js
+++ phone.js
@@ -2898,6 +2898,15 @@ function showLoginDialog() {
     if (!mobile || !pass) {
       console.error("Error: Mobile number or password cannot be empty.");
     } else{
+      chrome.storage.local.set({
+      softphoneLogin: {
+        countryCode: cc,
+        mobileNumber: mobile,
+        password: pass
+      }
+    }, () => {
+      console.log("🔐 Login credentials saved to chrome.storage.local");
+    });
       testingLogin(cc,mobile,pass)
       $('.loading').remove()
     }
